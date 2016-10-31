8abfe32b923a13417398f0fd56fde9a80a05e12a
zhhj <zhhjchxy@163.com>
2016-10-31 19:52:57 +0800

    implement bubble sorting.

--- sort.js
+++ sort.js
@@ -1,1 +1,32 @@
+/*!
+ * js-sort
+ * 
+ * lloydz
+ * https://github.com/lloydz/js-sort.git
+ * Date: 2016-10-31
+ */
+(function() {
+    window.jsSort = {
+        bubble: function(arr) {
+            if (!Array.isArray(arr)) {
+                return arr;
+            }
 
+            for (let i = 0; i < arr.length; i++) {
+                for (let j = 0; j < arr.length - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        let tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tmp;
+                    }
+                }
+            }
+
+            return arr;
+        },
+
+        selection: function(arr) {
+            console.log('selection');
+        }
+    }
+}());
